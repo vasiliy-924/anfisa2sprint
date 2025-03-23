431f6d4de89539d0c810f5f88c7ff21e0a7546ef
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-03-23 13:49:17 +0300

    after 8sprint

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,12 +6,13 @@ from ice_cream.models import IceCream
 def index(request):
     template_name = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description'
-    ).exclude(
-        is_published=True, is_on_main=True
+        'id', 'title', 'price', 'description'
+    ).filter(
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
     )
     context = {
         'ice_cream_list': ice_cream_list,
     }
     return render(request, template_name, context)
-
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,9 +4,12 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name = 'Название')
-    slug = models.SlugField(max_length=64, unique=True, verbose_name = 'Слаг')
-    output_order = models.PositiveSmallIntegerField(default=100, verbose_name = 'Порядок отображения')
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -17,8 +20,8 @@ class Category(PublishedModel):
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name = 'Название')
-    slug = models.SlugField(max_length=64, unique=True, verbose_name = 'Слаг')
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
 
     class Meta:
         verbose_name = 'топпинг'
@@ -27,25 +30,26 @@ class Topping(PublishedModel):
     def __str__(self):
         return self.title
 
+
 class Wrapper(PublishedModel):
     title = models.CharField(
-        max_length=256, 
-        verbose_name = 'Название', 
-        help_text = 'Уникальное название обёртки, не более 256 символов.'
+        max_length=256,
+        verbose_name='Название',
+        help_text='Уникальное название обёртки, не более 256 символов.'
     )
 
     class Meta:
         verbose_name = 'объект «Обёртка»'
         verbose_name_plural = 'Обёртки'
-    
+
     def __str__(self):
         return self.title
 
 
 class IceCream(PublishedModel):
-    is_on_main = models.BooleanField(default=False, verbose_name = 'На главную')
-    title = models.CharField(max_length=256, verbose_name = 'Название')
-    description = models.TextField(verbose_name = 'Описание')
+    is_on_main = models.BooleanField(default=False, verbose_name='На главную')
+    title = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -61,10 +65,16 @@ class IceCream(PublishedModel):
         verbose_name='Категория'
     )
     toppings = models.ManyToManyField(Topping, verbose_name='Топпинги')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
